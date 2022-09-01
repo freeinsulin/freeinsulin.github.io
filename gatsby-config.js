903f7e5bb8464c31ffa077b6a6635600878a3c3f
Matthew Feng <mattfeng@mit.edu>
2022-09-01 17:21:51 -0400

    start index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,15 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `The Free Insulin Project`,
+    description: `Insulin belongs to the people.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://freeinsulin.github.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,17 +20,11 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        typekit: {
+          id: process.env.TYPEKIT_ID,
+        },
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import * as styles from "../styles/index.module.scss"
+
 const IndexPage = () => (
   <Layout>
-    <h1>The Free Insulin Project</h1>
+    <div className={styles.index}>
+      <h1>Insulin belongs to the people.</h1>
+
+      <h2>A point-of-care insulin production system</h2>
+
+      <h3>The current regulatory framework</h3>
+      <p></p>
+
+      <h2>Always open and transparent</h2>
+
+      <h2>The road ahead</h2>
+      <h3>Funding</h3>
+
+      <h3>Funding</h3>
+    </div>
   </Layout>
 )
 export const Head = () => <Seo title="Home" />
